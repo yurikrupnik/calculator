7105f5a05aa8d4223e2d37f8eb5da522d034762c
yurikrupnik <krupnik.yuri@gmail.com>
2014-10-27 13:15:27 +0200

    clean up

--- calculator.js
+++ calculator.js
@@ -13,11 +13,11 @@
         input = document.getElementById("calc-input"),
         keyCodes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, // numbers 0-9
                     13, 42, 43, 45, 46, 47, 61],// enter, *, +, -, ., /, =
-        math = function (btn) {
+        math = function (operation) {
             if(!input.value) return; // prevent double operations in a row
             memory.innerHTML = memory.innerHTML.indexOf("&nbsp;") === 0 ? // remove &nbsp; for eval that will occur later
-            memory.innerHTML.slice(0,0) + input.value + btn :
-            memory.innerHTML + input.value + btn;
+            memory.innerHTML.slice(0,0) + input.value + operation :
+            memory.innerHTML + input.value + operation;
             input.value = "";
         },
         num = function (num) {
