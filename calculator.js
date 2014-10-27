6263034ccbe4e2bc0b9e539bacb4692230725815
yurikrupnik <krupnik.yuri@gmail.com>
2014-10-27 13:11:59 +0200

    clean up

--- calculator.js
+++ calculator.js
@@ -12,7 +12,17 @@
         memory = document.getElementById("memory-input"),
         input = document.getElementById("calc-input"),
         keyCodes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, // numbers 0-9
-                    13, 42, 43, 45, 46, 47, 61], // enter, *, +, -, ., /, =
+                    13, 42, 43, 45, 46, 47, 61],// enter, *, +, -, ., /, =
+        math = function (btn) {
+            if(!input.value) return; // prevent double operations in a row
+            memory.innerHTML = memory.innerHTML.indexOf("&nbsp;") === 0 ? // remove &nbsp; for eval that will occur later
+            memory.innerHTML.slice(0,0) + input.value + btn :
+            memory.innerHTML + input.value + btn;
+            input.value = "";
+        },
+        num = function (num) {
+            input.value += num;
+        },
         operations = {
             "=": function () {
                 if (!input.value || memory.innerHTML.indexOf("&nbsp") > -1) return; // return if input value is empty string or if no value stored in memory to evaluate
@@ -36,32 +46,24 @@
                 input.value = "";
                 memory.innerHTML = "&nbsp;";
             },
-            "+": math = function (btn) {
-                if(!input.value) return; // prevent double operations in a row
-                memory.innerHTML = memory.innerHTML.indexOf("&nbsp;") === 0 ? // remove &nbsp; for eval that will occur later
-                    memory.innerHTML.slice(0,0) + input.value + btn :
-                    memory.innerHTML + input.value + btn;
-                input.value = "";
-            },
-            "-": this.math,
-            "/": this.math,
-            "*": this.math,
+            "+": math,
+            "-": math,
+            "/": math,
+            "*": math,
             ".": function (dot) {
                 if(dot === "." && input.value.indexOf(".") >= 0) return; // prevent adding 2 decimals
                 input.value += dot;
             },
-            "1": num = function (num) {
-                input.value += num;
-            },
-            "2": this.num,
-            "3": this.num,
-            "4": this.num,
-            "5": this.num,
-            "6": this.num,
-            "7": this.num,
-            "8": this.num,
-            "9": this.num,
-            "0": this.num
+            "1": num,
+            "2": num,
+            "3": num,
+            "4": num,
+            "5": num,
+            "6": num,
+            "7": num,
+            "8": num,
+            "9": num,
+            "0": num
         };
 
     function btn_click() {
